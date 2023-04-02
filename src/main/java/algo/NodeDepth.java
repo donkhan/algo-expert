a1374044904f69f679787cb279639132eba9df45
panoptic-qs-kamil <kamil@panoptic-qs.com>
2023-04-02 14:49:02 +0100

    Node depth done

--- src/main/java/algo/NodeDepth.java
+++ src/main/java/algo/NodeDepth.java
@@ -2,11 +2,16 @@ package algo;
 
 import java.util.*;
 
-class NodeDepth {
+public class NodeDepth {
+
+    static int depth(BinaryTree r, int n){
+        if(r == null) return 0;
+        return n + depth(r.left,n + 1) + depth(r.right, n+1);
+    }
 
     public static int nodeDepths(BinaryTree root) {
         // Write your code here.
-        return -1;
+        return depth(root,0);
     }
 
     static class BinaryTree {
@@ -19,5 +24,22 @@ class NodeDepth {
             left = null;
             right = null;
         }
+    }
+
+    public static void main(String args[]){
+        BinaryTree n1 = new BinaryTree(1);
+        BinaryTree n2 = new BinaryTree(2);
+        BinaryTree n3 = new BinaryTree(3);
+        BinaryTree n4 = new BinaryTree(4);
+        BinaryTree n5 = new BinaryTree(5);
+        BinaryTree n6 = new BinaryTree(6);
+        BinaryTree n7 = new BinaryTree(7);
+        BinaryTree n8 = new BinaryTree(8);
+        BinaryTree n9 = new BinaryTree(9);
+        n1.left = n2; n1.right = n3;
+        n2.left = n4; n2.right = n5;
+        n4.left = n8; n4.right = n9;
+        n3.left = n6; n3.right = n7;
+        System.out.println(nodeDepths(n1));
     }
 }
