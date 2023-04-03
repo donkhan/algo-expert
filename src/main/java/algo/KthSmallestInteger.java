4b44e15444441837618ad6e49e4a2ba2a04ea308
panoptic-qs-kamil <kamil@panoptic-qs.com>
2023-04-03 06:51:00 +0100

    Kth Smallest

--- src/main/java/algo/KthSmallestInteger.java
+++ src/main/java/algo/KthSmallestInteger.java
@@ -1,2 +1,18 @@
-package algo;public class KthSmallestInteger {
+package algo;
+import java.util.*;
+
+public class KthSmallestInteger {
+
+
+        public static int quickselect(int[] array, int k) {
+            if(array.length < k) return -1;
+            Arrays.sort(array);
+            return array[k-1];
+        }
+
+
+        public static  void main(String args[]){
+            System.out.println(quickselect(new int[]{8,5,2,9,7,6,3},3));
+        }
+
 }
