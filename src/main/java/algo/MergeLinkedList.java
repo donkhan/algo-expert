e47780203adefb577f2b571dfd3e87693778e5e1
panoptic-qs-kamil <kamil@panoptic-qs.com>
2023-04-04 18:55:03 +0100

    Merging of Linked List done

--- src/main/java/algo/MergeLinkedList.java
+++ src/main/java/algo/MergeLinkedList.java
@@ -14,35 +14,60 @@ public class MergeLinkedList {
         }
     }
 
-    public LinkedList mergingLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo) {
-        LinkedList t1 = linkedListOne;
-        while(t1 != null){
-            LinkedList t2 = linkedListTwo;
-            while(t2 != null){
-                if(t1 == t2) return t1;
-                t2 = t2.next;
-            }
-            t1 = t1.next;
+    public LinkedList mergingLinkedLists(LinkedList t1, LinkedList t2) {
+        LinkedList head = null;
+        if(t1 == null && t2 == null){
+            return head;
         }
-        return null;
+        if(t1 == null){
+            head = t2;
+            head.next = mergingLinkedLists(t1,t2.next);
+            return head;
+        }
+        if(t2 == null){
+            head = t1;
+            head.next = mergingLinkedLists(t1.next,t2);
+            return head;
+        }
+        if(t1.value < t2.value){
+            head = t1;
+            head.next = mergingLinkedLists(t1.next,t2);
+        }
+        else {
+            head = t2;
+            head.next = mergingLinkedLists(t1,t2.next);
+        }
+        return head;
+
+    }
+
+    public void traverse(LinkedList head){
+        LinkedList tmp = head;
+        while(tmp != null){
+            System.out.print(tmp.value + " ");
+            tmp = tmp.next;
+        }
+        System.out.println();
     }
 
     public static void main(String args[]){
         MergeLinkedList mll = new MergeLinkedList();
-        LinkedList l1 = new LinkedList(2);
-        LinkedList l2 = new LinkedList(3);
-        LinkedList l3 = new LinkedList(1);
+        LinkedList l2 = new LinkedList(2);
+        LinkedList l6 = new LinkedList(6);
+        LinkedList l7 = new LinkedList(7);
+        LinkedList l8 = new LinkedList(8);
+        l2.next = l6; l6.next = l7; l7.next = l8;
+        mll.traverse(l2);
+
+        LinkedList l1 = new LinkedList(1);
+        LinkedList l3 = new LinkedList(3);
         LinkedList l4 = new LinkedList(4);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
+        LinkedList l5 = new LinkedList(5);
+        LinkedList l9 = new LinkedList(9);
+        LinkedList l10 = new LinkedList(10);
+        l1.next = l3; l3.next  = l4; l4.next = l5; l5.next = l9;l9.next = l10;
 
-        LinkedList l21 = new LinkedList(8);
-        LinkedList l22 = new LinkedList(7);
-        l21.next = l22;
-        l22.next = l3;
-
-
-        System.out.println(mll.mergingLinkedLists(l1,l21).value);
+        mll.traverse(l1);;
+        mll.traverse(mll.mergingLinkedLists(l1,l2));
     }
 }
